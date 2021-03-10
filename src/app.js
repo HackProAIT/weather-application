a13dc2007c400baddb23b38fcb0178768547faef
vijay3778 <dididivya999@gmail.com>
2021-03-11 00:04:19 +0530

    app ready for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express()
+const port = process.env.PORT || 3000
 const path = require('path')
 
 //path for express config
@@ -69,6 +70,6 @@ app.get('*', (req, res) => {
     res.send('My 404 page')
 })
 
-app.listen(3000, () => {
-    console.log('server running at port -> 3000')
+app.listen(port, () => {
+    console.log('server running at port -> ' + port)
 })
